4b4e889411c7ae073a506dfa31d9e531ff01bf6b
Mayara Sarmento Paes <mpaesps133@gmail.com>
2024-10-02 13:52:57 -0300

    conexao db

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js"
+import mongoose from "mongoose";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) =>{
+    console.error("erro de conexao", erro);
+})
+
+conexao.once("open", () =>{
+    console.log("Conexão com banco estabelecida")
+})
 
 const app = express();
 app.use(express.json()); //middleware acesso as reqiuiçoes e respostas e fazer açoes nela
-
-const livros = [
-    {
-        "id" : 1,
-        "titulo" : "Crepúsculo"
-    },
-    {
-         "id" : 2,
-        "titulo" : "Maus"
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro =>{
-        return livro.id === Number(id);
-    })
-}
 
 app.get("/", (req, res) => {
     res.status(200).send("oiiiiii");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) =>{
@@ -53,3 +50,4 @@ app.delete("/livros/:id", (req, res) => {
 export default app;
 
 //callback function
+
